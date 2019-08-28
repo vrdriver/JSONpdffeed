09a773d34f7a0cc93ebbc9d35588bd732a7ec05e
Steve <steve@bluetomatomedia.com>
2019-08-28 21:59:56 +1000

    Update json.php
    Removed all the ID3/mp3 Tag garbage from previous versions and cleaned it up.

--- json.php
+++ json.php
@@ -1,25 +1,18 @@
 <?php 
-
-error_reporting(E_ALL); 
-ini_set('display_errors', '1');
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
 
 /*
  * A JSON Feed to display PDF files.
  * Updated by Stephen Monro
  * bluetomatomedia.com
- *
- * NOPE ---- iTunes-Compatible RSS 2.0 MP3 subscription feed script ----
+ * 
  * Original work by Rob W of http://www.podcast411.com/
  * Updated by Aaron Snoswell (aaronsnoswell@gmail.com)
- *
- * Recurses a given directory, reading MP3 ID3 tags and generating an itunes
- * compatible RSS podcast feed.
+ * 
  *
  * Save this .php file wherever you like on your server. The URL for this .php
-*/
-
-//error_reporting(E_ALL); 
-//ini_set('display_errors', '1');
+*/ 
 
 /* 
  * CONFIGURATION VARIABLES:
@@ -30,7 +23,7 @@ ini_set('display_errors', '1');
  */
 // ============================================
 // General Configuration Options
-// Location of MP3's on server. TRAILING SLASH REQ'D.
+// Location of PDF's on server. TRAILING SLASH REQ'D.
 $files_dir = "/home/yoursite.com/apps/pdf/";
 $files_dir = getcwd().'/';
 // Corresponding url for accessing the above directory. TRAILING SLASH REQ'D.
@@ -42,83 +35,20 @@ if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
 }
 $base_url = $protocol . $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']);
 $files_url = $base_url.'/';
-// Location of getid3 folder, leave blank to disable. TRAILING SLASH REQ'D.
-$getid3_dir = "getid3/";
-$getid3_dir = "";
-
-
+  
 // ====================================================== Generic feed options
 // Your feed's title
-$feed_title = "Your Newspaper json list";
+$feed_title = "Your PDF json list";
 // 'More info' link for your feed
 $feed_link = "https://resources.yoursite.com/apps/pdf/";
 // homepage
-$home_page = "https://c4israel.org";
+$home_page = "https://yoursites.com";
 // Brief description
 $feed_description = "";
-// Copyright / license information
-$feed_copyright = "All content &#0169; You" . date("Y");
-// How often feed readers check for new material (in seconds) -- mostly ignored by readers
-$feed_ttl = 60 * 60 * 24;
-// Language locale of your feed, eg en-us, de, fr etc. See http://www.rssboard.org/rss-language-codes
-$feed_lang = "en-au";
-// ============================================== iTunes-specific feed options
 // You, or your organisation's name
-$feed_author = "You";
-// Feed author's contact email address
-$feed_email="";
-// Url of a 170x170 .png image to be used on the iTunes page
-$feed_image = "";
-// If your feed contains explicit material or not (yes, no, clean)
-$feed_explicit = "clean";
-// iTunes major category of your feed
-$feed_category = "News";
-// iTunes minor category of your feed
-$feed_subcategory = "news";
+$feed_author = "You"; 
 // END OF CONFIGURATION VARIABLES
-// If getid3 was requested, attempt to initialise the ID3 engine
-$getid3_engine = NULL;
-
-if(strlen($getid3_dir) != 0) {
-    require_once($getid3_dir . 'getid3.php');
-    $getid3_engine = new getID3;
-}
-/*
-// Write XML heading
- echo '<?xml version="1.0" encoding="utf-8" ?>';
-
-
-<!-- generator="awesome-sauce/1.1" -->
-
-<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
-
-    <channel>
-        <title><? echo $feed_title; ?></title>
-        <link><? echo $feed_link; ?></link>
-
-        <!-- iTunes-specific metadata -->
-        <itunes:author><? echo $feed_author; ?></itunes:author>
-        <itunes:owner>
-            <itunes:name><? echo $feed_author; ?></itunes:name>
-            <itunes:email><? echo $feed_email; ?></itunes:email>
-        </itunes:owner>
-
-        <itunes:image href="<? echo $feed_image; ?>" />
-        <itunes:explicit><? echo $feed_explicit; ?></itunes:explicit>
-        <itunes:category text="<? echo $feed_category; ?>">
-            <itunes:category text="<? echo $feed_subcategory; ?>" />
-        </itunes:category>
-
-        <itunes:summary><? echo $feed_description; ?></itunes:summary>
-
-        <!-- Non-iTunes metadata -->
-        <category>Music</category>
-        <description><? echo $feed_description; ?></description>
-        
-        <language><? echo $feed_lang; ?></language>
-        <copyright><? echo $feed_copyright; ?></copyright>
-        <ttl><? echo $feed_ttl; ?></ttl>
-*/
+  
 ?>
 {
     "version": "https://jsonfeed.org/version/1",
@@ -142,41 +72,17 @@ if(strlen($getid3_dir) != 0) {
 	    
             if(is_file($file_path) && strrchr($file_path, '.') == ".pdf")
 	    {
-                // Initialise file details to sensible defaults
-                $file_title = $file;
-                $file_url = $files_url . $file;
-                $file_author = $feed_author;
-                $file_duration = "";
-                $file_description = "";
-                $file_date = date(DateTime::RFC2822, filemtime($file_path));
-                $file_size = filesize($file_path);
-                
-                $basename = basename($file_path);
-                
-                // Read file metadata from the ID3 tags
-                if(!is_null($getid3_engine))
-		{
-                    $id3_info = $getid3_engine->analyze($file_path);
-                    getid3_lib::CopyTagsToComments($id3_info);
-                    
-                    
-                    if(isset($id3_info["comments_html"]["title"][0]))
-                    {
-                    $file_title = $id3_info["comments_html"]["title"][0];
-                    } else  $file_title  = "";
-                    
-                    
-			if(isset($id3_info["comments_html"]["artist"][0]))
-			{
-				$file_author = $id3_info["comments_html"]["artist"][0];
-			} else  $file_title  = "";
-			
-			if(isset($id3_info["playtime_string"]))
-			{
-				$file_duration = $id3_info["playtime_string"];
-			} else  $file_title  = "";
-	}
-		echo $tooutput . "\n";
+            // Initialise file details to sensible defaults
+            $file_title = $file;
+            $file_url = $files_url . $file;
+            $file_author = $feed_author;
+            $file_duration = "";
+            $file_description = "";
+            $file_date = date(DateTime::RFC2822, filemtime($file_path));
+            $file_size = filesize($file_path);
+            
+            $basename = basename($file_path); 
+		    echo $tooutput . "\n";
 	?>
         {
             "id": "",
